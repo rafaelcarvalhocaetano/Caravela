fe19c34b36774aec571b4918818492eb612bf95d
Rafael Carvalho <rapha.pse@outlook.com>
2020-01-24 21:31:06 -0300

    Finished Card

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vc-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { MenuComponent } from './components/menu/menu.component';
 import { TableComponent } from './components/table/table.component';
-
-
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     MenuComponent,
-    TableComponent
+    TableComponent,
+    CardComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     MenuComponent,
-    TableComponent
+    TableComponent,
+    CardComponent
   ]
 })
 export class SharedModule { }
